5467f6fc29b1a3f37b3e695b1d6e7ac78d58c3f9
yurysakovich-fr <yury.sakovich@globalwork.ai>
2025-11-22 22:53:23 +0300

    add mail

--- src/components/EighthBlock/EighthBlock.jsx
+++ src/components/EighthBlock/EighthBlock.jsx
@@ -2,12 +2,16 @@ import styles from './EighthBlock.module.scss'
 import vectorSvg from '../../../assets/svg/step8/vector.svg'
 
 function EighthBlock() {
+  const handleWriteUsClick = () => {
+    window.location.href = 'mailto:info@emeraldglad.com'
+  }
+
   return (
     <div id="contacts" className={styles.eighthBlock}>
       <div className={styles.content}>
         <h2 className={styles.title}>EXPERT SUPPORT <br/> TO ACHIEVE BUSINESS GOALS</h2>
         
-        <button className={styles.writeUsButton}>WRITE US</button>
+        <button className={styles.writeUsButton} onClick={handleWriteUsClick}>WRITE US</button>
         
         <div className={styles.logoContainer}>
           <img src={vectorSvg} alt="Arrow" className={styles.logoIcon} />
