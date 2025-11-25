0df440530e4aaf51a4fd194c330be96e749e1915
yurysakovich-fr <yury.sakovich@globalwork.ai>
2025-11-26 00:48:41 +0300

    updates

--- src/components/EighthBlock/EighthBlock.jsx
+++ src/components/EighthBlock/EighthBlock.jsx
@@ -1,25 +1,19 @@
-import { useRef } from 'react'
 import styles from './EighthBlock.module.scss'
 import vectorSvg from '../../../assets/svg/step8/vector.svg'
 
 function EighthBlock() {
-  const blockRef = useRef(null)
-  const titleRef = useRef(null)
-  const buttonRef = useRef(null)
-  const logoRef = useRef(null)
-
   const handleWriteUsClick = () => {
     window.location.href = 'mailto:info@emeraldglad.com'
   }
 
   return (
-    <div ref={blockRef} id="contacts" className={styles.eighthBlock}>
+    <div id="contacts" className={styles.eighthBlock}>
       <div className={styles.content}>
-        <h2 ref={titleRef} className={styles.title}>EXPERT SUPPORT <br/> TO ACHIEVE BUSINESS GOALS</h2>
+        <h2 className={styles.title}>EXPERT SUPPORT <br/> TO ACHIEVE BUSINESS GOALS</h2>
         
-        <button ref={buttonRef} className={styles.writeUsButton} onClick={handleWriteUsClick}>WRITE US</button>
+        <button className={styles.writeUsButton} onClick={handleWriteUsClick}>WRITE US</button>
         
-        <div ref={logoRef} className={styles.logoContainer}>
+        <div className={styles.logoContainer}>
           <img src={vectorSvg} alt="Arrow" className={styles.logoIcon} />
         </div>
       </div>
